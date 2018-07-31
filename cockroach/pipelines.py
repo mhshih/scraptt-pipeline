c1d39bcdfc8c1f66338a10635038715ee49ff2b3
amigcamel <amigcamel@gmail.com>
2018-07-31 14:23:59 +0800

    add ElasticsearchPipeline

--- cockroach/pipelines.py
+++ cockroach/pipelines.py
@@ -4,6 +4,7 @@ from hashlib import sha256
 import logging
 
 from .db import Session, Post, Comment, Meta
+from .es import CockroachDoc
 
 logger = logging.getLogger(__name__)
 
@@ -72,4 +73,40 @@ class MetaPipeline(BasePipeline):
         )
         self.session.merge(meta_obj)
         self.session.commit()
+        return item
+
+
+class ElasticsearchPipeline:
+    """Insert PTT POST and COMMENT into Elasticsearch."""
+
+    def process_item(self, item, spider):
+        """Insert data into database."""
+        CockroachDoc(
+            post_type=0,
+            board=item['board'],
+            author=item['author'],
+            published=item['time']['published'],
+            title=item['title'],
+            content=item['content'],
+            ip=item['ip'],
+            upvote=item['count']['推'],
+            novote=item['count']['→'],
+            downvote=item['count']['噓'],
+            id=item['id'],
+        ).save()
+        for comment in item['comments']:
+            hashid = sha256((
+                f"{item['id']}"
+                f"{comment['author']}"
+                f"{comment['time']['published']}"
+            ).encode('utf-8')).hexdigest()[:16]
+            CockroachDoc(
+                id=hashid,
+                type=comment['type'],
+                author=comment['author'],
+                published=comment['time']['published'],
+                ip=comment['ip'],
+                content=comment['content'],
+                post_id=item['id'],
+            ).save()
         return item
